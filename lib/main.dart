1939c6f252ed197b9196eba77474e3d57e9f88f3
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-06-19 22:54:11 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,12 @@ void main() {
           backgroundColor: Colors.blueGrey[900],
 
         ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80'),
+
+          ),
+        ),
 
       ),
 
