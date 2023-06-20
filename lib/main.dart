5cb3da67a2c61fc3b4cc8fe9c5708d7fcc62dd90
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-06-20 22:28:21 +0530

    made changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,12 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage('https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80'),
-
-          ),
+            image: AssetImage('images/img.png'),
         ),
 
       ),
 
+    ),
     ),
   );
 }
