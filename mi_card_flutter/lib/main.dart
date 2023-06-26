cc1d7afafd60f332cd8ed5ff0945cf0313576a69
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-06-26 22:08:37 +0530

    made changes

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -13,7 +13,15 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
         home:Scaffold(
           backgroundColor: Colors.teal,
-          body:Container(),
+          body: SafeArea(
+            child: Container(
+              height: 100.0,
+              width:100.0,
+              margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+              color:Colors.white,
+              child: Text("hello"),
+            ),
+          ),
         )
     );
   }
