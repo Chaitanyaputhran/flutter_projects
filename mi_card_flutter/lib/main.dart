46a46960af275da5b299de16cc7041f065871c05
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-06-29 13:56:34 +0530

    added more properties

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-   MyApp()
+  runApp(MyApp()
   );
 }
 class MyApp extends StatelessWidget {
@@ -10,34 +9,106 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ImageProvider image;
     return  MaterialApp(
         home:Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-            child:Column(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleAvatar(
                   radius: 50.0,
-                  backgroundImage: AssetImage('/images/chaitanya.jpg'),
+                  backgroundImage: AssetImage('/assets/chaitanya1.jpg'),
                 ),
                 Text(
-                  style:TextStyle(
-                    fontSize: 40.0,
-                    color:Colors.white
-                      font weight:font-weight.bold
-                  )
                   'Chaitanya',
-                  Text()
-                )
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+
+                  ),
+                ),
+                Text(
+                  'FRONTEND DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'SourceSan3',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                    letterSpacing: 2.5,
+                  ),
+
+                ),
+                SizedBox(
+                  height:20.0,
+                  width:150.0,
+                  child:Divider(
+                    color:Colors.teal.shade100,
+
+                  )
+                ),
+                Card(
+
+
+                  margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                  child:Padding(
+                    padding:EdgeInsets.all(25.0),
+                    child:ListTile(
+                      leading:Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title:Text('8792620374',
+                          style: TextStyle(
+                            color: Colors.teal.shade800,
+                            fontFamily: 'SourceSan3',
+                            fontSize: 20.0,
+
+                          )
+                      )
+                    )
+                  ),
+                ),
+
+                Card(
+                    margin:EdgeInsets.symmetric(vertical:10.0,horizontal:25.0),
+                    child:Padding(
+                      padding:EdgeInsets.all(10.0),
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child:ListTile(
+                          leading:Icon(
+                            Icons.email,
+                            color: Colors.teal,
+                          ),
+                          title:Text('chaitanyaputhran4@gmail.com',
+                              style: TextStyle(
+                                color: Colors.teal.shade800,
+                                fontFamily: 'SourceSan3',
+                                fontSize: 20.0,
+
+                              )
+                          )
+                        )
+                      ),
+                    ),
+                ),
+
+
+
+
+
 
 
               ],
             ),
 
           ),
-        )
+        ),
     );
   }
 }
-
 
