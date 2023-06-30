2cf9fda21cc1363d2ac6e25cb31c84fdb762ab3a
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-06-30 22:35:08 +0530

    updated

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -18,6 +18,32 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child:Row(
+      children:<Widget>[
+        Expanded(
+
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice1.png'),
+          ),
+
+          ),
+
+
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset('images/dice1.png'),
+          ),
+          ),
+
+
+
+
+      ],
+    ),
+    );
   }
 }
+
