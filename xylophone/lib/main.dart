a3ca70bee2f7305427cc95fbf5c12ff2ae8b1b2e
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-03 19:45:20 +0530

    added xylophone

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:audiofileplayer/audiofileplayer.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
