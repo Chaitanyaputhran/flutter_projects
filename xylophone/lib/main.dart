37484a253eb7243311a85ee4e0a13df4e3c6ee00
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-03 21:13:44 +0530

    updated

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,17 +1,106 @@
 import 'package:flutter/material.dart';
-import 'package:audiofileplayer/audiofileplayer.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int Sound){
+    final assetsAudioPlayer = AssetsAudioPlayer();
+
+    assetsAudioPlayer.open(
+      Audio("assets/note$Sound.wav"),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+            child:Column(
+              children: [
+                Expanded(
+            child:TextButton(
+                  style:TextButton.styleFrom(
+                backgroundColor: Colors.pink,
+            ),
+                  onPressed: (){
+                    playSound(1);
+                },
+              child:Text(''),
+            ),
+                ),
+                Expanded(
+                  child:TextButton(
+                    style:TextButton.styleFrom(
+                      backgroundColor: Colors.pink,
+                    ),
+                    onPressed: (){
+                      playSound(2);
+                    },
+                    child:Text(''),
+                  ),
+                ),
+                Expanded(
+                  child:TextButton(
+                    style:TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    onPressed: (){
+                      playSound(3);
+                    },
+                    child:Text(''),
+                  ),
+                ),
+                Expanded(
+                  child:TextButton(
+                    style:TextButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                    ),
+                    onPressed: (){
+                      playSound(4);
+                    },
+                    child:Text(''),
+                  ),
+                ),
+                Expanded(
+                  child:TextButton(
+                    style:TextButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                    onPressed: (){
+                      playSound(5);
+                    },
+                    child:Text(''),
+                  ),
+                ),
+                Expanded(
+                  child:TextButton(
+                    style:TextButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                    ),
+                    onPressed: (){
+                      playSound(6);
+                    },
+                    child:Text(''),
+                  ),
+                ),
+                Expanded(
+                  child:TextButton(
+                    style:TextButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
+                    onPressed: (){
+                      playSound(7);
+                    },
+                    child:Text(''),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
         ),
-      ),
+
     );
   }
 }
