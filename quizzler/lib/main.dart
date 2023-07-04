cec9318f92eb14136c6c8402eff82107a70f7363
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-04 20:15:25 +0530

    added question_brain.dart

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -28,11 +28,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scorekeeper = [];
 
-List<Question>questionBank=[
-Question(q:'You can lead a cow down stairs but not up stairs.',a: false),
-  Question(q:'Approximately one quarter of human bones are in the feet.',a:true),
-  Question(q: 'A slug\'s blood is green.',a:true),
-];
   int questionNumber=0;
   @override
   Widget build(BuildContext context) {
