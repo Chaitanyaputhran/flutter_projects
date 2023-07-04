84187ece26849dcc2e692ffe98cf248410bfc848
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-04 19:59:04 +0530

    updated

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -30,7 +30,7 @@ class _QuizPageState extends State<QuizPage> {
 
 List<Question>questionBank=[
 Question(q:'You can lead a cow down stairs but not up stairs.',a: false),
-  Question(q:'Approximately one quarter of human bones are in the feet.',a:true);
+  Question(q:'Approximately one quarter of human bones are in the feet.',a:true),
   Question(q: 'A slug\'s blood is green.',a:true),
 ];
   int questionNumber=0;
@@ -105,7 +105,7 @@ Question(q:'You can lead a cow down stairs but not up stairs.',a: false),
                 ),
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == false){
                   print("user got it right");
                 }
