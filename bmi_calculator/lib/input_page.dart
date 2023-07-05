3dd598cfc74604e0e4523e61305f8d4f9991a643
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-05 23:14:42 +0530

    added 2 more files

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import'icon_content.dart';
+import'reusable_dart.dart';
 const bottomContainerHeight= 80.0;
 const activeCardColor =Color(0xFF1D1E33);
 const bottomContainerColour = Color(0xFFEB1555);
@@ -24,11 +27,19 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                     child:ResuableCard(
           colour: activeCardColor,
+                      cardChild: IconContent(
+                        icon:FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
       ),
                 ),
                 Expanded(
                     child:ResuableCard(
                       colour: activeCardColor,
+                      cardChild:IconContent(
+                        icon:FontAwesomeIcons.mars,
+                        label:'FEMALE',
+                      ),
                 ),
                 ),
               ],
@@ -69,20 +80,5 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class ResuableCard extends StatelessWidget {
-  ResuableCard({required this.colour});
-  final Color colour;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin:EdgeInsets.all(15.0),
-
-      decoration:BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-}
 
---
+++ bmi_calculator/lib/reusable_dart.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+class ResuableCard extends StatelessWidget {
+  ResuableCard({required this.colour,required this.cardChild});
+  final Color colour;
+  final Widget cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin:EdgeInsets.all(15.0),
+
+      decoration:BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
