8ec84f5aa6a1ce77b4145e7bd28fe959900dd70e
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-05 16:17:33 +0530

    updated

---
+++ bmi_calculator/lib/input_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
