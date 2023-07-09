a9231a248f1435b20e3966fafc7f344b34883e07
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-09 20:13:33 +0530

    updated

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -26,7 +27,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
     http.Response response = await http.get(url);
     if (response.statusCode == 200){
       String data = response.body;
-      print(data);
+      var decodeData = jsonDecode(data);
+
+     double temperature= decodeData['main']['temp'];
+      print(temperature);
+      int condition =decodeData['weather'][0]['id'];
+      print(condition);
+      String cityName =decodeData['name'];
+      print(cityName);
     }else{
       print(response.statusCode);
     }
