43e553c40254c2d515406d3c226702751a4235ba
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-07-09 13:19:43 +0530

    updated

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -13,14 +13,36 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
   void getLocation() async{
 
+    try{
 
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
 
     print(position);
   }
+  catch (e) {
+      print(e);
+  }}
+  void somethingThatExpectsLessThan10(int n){
+    if (n>10){
+      throw 'n is greater than 10,n should always be less than 10.';
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    String myMargin = '15';
+    double myMarginAsDouble=30.0;
 
+    try {
+      myMarginAsDouble = double.parse(myMargin);
+    } catch (e) {
+      print(e);
+    }
+
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(myMarginAsDouble ?? 30.0),
+        color: Colors.red,
+      ),
+    );
   }
 }
